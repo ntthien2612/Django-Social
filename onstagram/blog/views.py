2150f2c7802db77e15bd1d8b7ca7c8211a1b9497
ntthien2612 <ntthien.work.2612@gmail.com>
2022-10-28 13:59:31 +0700

    thien

--- onstagram/blog/views.py
+++ onstagram/blog/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from .models import Comment, Post 
 """ Home page with all posts """
 
+
+def home(request):
+    
+    return render(
+        request ,
+        "blog/home.html",)
+
 def dashboard(request):
     
     return render(
