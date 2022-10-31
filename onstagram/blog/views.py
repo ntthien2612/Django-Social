ef03715406398aa561973e21993370c93f479002
ntthien2612 <ntthien.work.2612@gmail.com>
2022-10-31 15:47:22 +0700

    thien up

--- onstagram/blog/views.py
+++ onstagram/blog/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from .models import Comment, Post 
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth.models import User
 """ Home page with all posts """
 
-
+@login_required
 def home(request):
     
+    posts = Post.objects.all()
     return render(
-        request ,
-        "blog/home.html",)
+        request,
+        "blog/home.html",
+        {"blogs": posts}
+    )
 
+@login_required
 def dashboard(request):
     
     return render(
@@ -18,7 +22,6 @@ def dashboard(request):
         "blog/first.html",)
 @login_required
 def about(request):
-    
-    return render(
-        request ,
-        "blog/about.html",)
+    return render(request, 'blog/about.html', {'title':'About'})
+
+
